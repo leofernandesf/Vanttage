fdaa18495591b5047b108f17e8a1864da6674c22
leonardo fernandes farias <leonardo.fernandesf@gmail.com>
2016-09-30 08:29:56 -0400

    Initial Commit

---
+++ Vanttage-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vanttage-v2
+//
+//  Created by leo on 30/09/16.
+//  Copyright © 2016 tap4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
